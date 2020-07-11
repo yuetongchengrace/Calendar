c4d0a52c4b6f2a37de189083c68fcc276e2c433e
Qichenye <59810094+Qichenye@users.noreply.github.com>
2020-07-10 23:50:18 -0500

    host previlege added

---
+++ checkHost.php
@@ -0,0 +1,39 @@
+<?php
+    session_start();
+    header("Content-Type: application/json"); 
+    $json_str = file_get_contents('php://input');
+    //This will store the data into an associative array
+    $json_obj = json_decode($json_str, true);
+    $id = $json_obj['id'];
+    $user = $_SESSION['username'];
+
+    require 'database.php';
+    $stmt = $mysqli->prepare("select host from group_events where event_id='$id'");
+    if(!$stmt){
+        echo json_encode(array(
+            "success" => false,
+            "message" => "Query failed."
+        ));
+        exit;
+    }
+
+    $stmt->execute();
+    $result= $stmt->get_result();
+    $row = $result->fetch_assoc();
+    $host = $row['host'];
+    $stmt->close();
+
+    if($host == $user){
+        echo json_encode(array(
+            "success" => true,
+            "message" => "permitted"
+        ));
+    }
+    else{
+        echo json_encode(array(
+            "success" => false,
+            "message" => "permission denied"
+        ));
+    }
+    exit;
+?>
